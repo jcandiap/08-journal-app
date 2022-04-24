0d1acfc177d6de78515746d2fcd746907a8c4f00
Jose Candia <josecandiap@gmail.com>
2022-04-23 23:36:44 -0400

    inicio con redux

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { legacy_createStore as createStore, combineReducers} from 'redux'
+import { authReducer } from '../reducers/authReducer';
+
+const reducers = combineReducers({
+    auth: authReducer
+});
+
+export const store = createStore(reducers);
